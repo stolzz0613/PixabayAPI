5e49dc3d850682114516ac82069be33ce63ad349
stolzz0613 <andres0613@utp.edu.co>
2020-07-23 02:26:49 -0500

    Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from "../components/Error";
 
 const Formulario = () => {
+
+    const [termino, setTermino] = useState("");
+    const [error, setError] = useState(false);
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        if (termino.trim() === "") {
+            setError(true);
+            return;
+        }
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen: ejemplo: futbol o café"
+                        onChange={e => setTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,6 +36,12 @@ const Formulario = () => {
                     />
                 </div>
             </div>
+            {error
+                ? <Error
+                    mensaje="Agrega un término de búsqueda"
+                />
+                : null
+            }
         </form>
     );
 }
